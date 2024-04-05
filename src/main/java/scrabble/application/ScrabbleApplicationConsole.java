b7d9aff7747ce8c44da6de37d6d42d5407c76e9b
Hugo H. <51268820+hugoheml@users.noreply.github.com>
2024-04-05 09:01:49 +0200

    feat: add new console log on ScrabbleApplicationConsole

--- src/main/java/scrabble/application/ScrabbleApplicationConsole.java
+++ src/main/java/scrabble/application/ScrabbleApplicationConsole.java
@@ -1,11 +1,12 @@
 package scrabble.application;
 
 public class ScrabbleApplicationConsole {
-	public static String SEPARATOR = "------------------------------------------------------";
+	public static String SEPARATOR = "-------------------------------------------------------";
 
 	public static void main(String[] args) {
 		System.out.println(SEPARATOR);
-		System.out.println("-- Bienvenu dans notre magnifique jeu de scrabble ! --");
+		System.out.println("-- Bienvenue dans notre magnifique jeu de scrabble ! --");
+		System.out.println("-- développé par Hugo                                --");
 		System.out.println(SEPARATOR);
 
 	}
