a12f8d777bca41828ce96f5e56e19e0a205b330d
Lucas Bocquet <lucas.bocquet@etu.unilim.fr>
2024-04-05 08:43:38 +0200

    add ScrabbleApplicationConsole

---
+++ src/main/java/scrabble/application/ScrabbleApplicationConsole.java
@@ -0,0 +1,13 @@
+package scrabble.application;
+
+public class ScrabbleApplicationConsole {
+	public static String SEPARATOR = "------------------------------------------------------";
+
+	public static void main(String[] args) {
+		System.out.println(SEPARATOR);
+		System.out.println("-- Bienvenu dans notre magnifique jeu de scrabble ! --");
+		System.out.println(SEPARATOR);
+
+	}
+
+}
