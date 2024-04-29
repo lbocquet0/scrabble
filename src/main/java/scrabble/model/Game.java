48f9b0f074e52a3d058b6e373e6f8427d25a5864
Eliott Bayart <eliott.bayart@etu.unilim.fr>
2024-04-29 11:47:34 +0200

    feat: add class game with getters

---
+++ src/main/java/scrabble/model/Game.java
@@ -0,0 +1,22 @@
+package scrabble.model;
+
+public class Game {
+	
+	private Player player;
+	private Board board;
+	private Bag bag;
+	
+	public Game() {
+		Player player = new Player();
+		Board board = new Board();
+		Bag bag = new Bag();
+	}
+	
+	public Board getBoard() {
+		return this.board;
+	}
+	
+	public Bag getBag() {
+		return this.bag;
+	}
+}
