a677102805d76599ca0b252edebde7a1fe55163a
Hugo H. <51268820+hugoheml@users.noreply.github.com>
2024-05-06 17:45:38 +0200

    feat: add askInt method in Console

--- src/main/java/scrabble/model/Console.java
+++ src/main/java/scrabble/model/Console.java
@@ -1,7 +1,27 @@
 package scrabble.model;
 
+import java.util.Scanner;
+
 public class Console {
 	public static void message(String message) {
 		System.out.println(message);
+	}
+
+	public static Integer askInt(String question, Integer min, Integer max) {
+		Scanner scanner = new Scanner(System.in);
+		Console.message(question);
+
+		Integer response = null;
+		while (response == null) {
+			response = scanner.nextInt();
+			if (response < min || response > max) {
+				Console.message("Veuillez saisir un nombre entre " + min + " et " + max);
+				response = null;
+			}
+		}
+
+		scanner.close();
+
+		return response;
 	}
 }
