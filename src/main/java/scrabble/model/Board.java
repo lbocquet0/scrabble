63e35b645a12d7d6719c23360e92800dc1354014
Eliott Bayart <eliott.bayart@etu.unilim.fr>
2024-05-06 11:29:39 +0200

    feat: add display token on board

--- src/main/java/scrabble/model/Board.java
+++ src/main/java/scrabble/model/Board.java
@@ -23,28 +23,44 @@ public class Board {
 		}
 	}
 	
-	public static void display() {
-	    final int NB_LIGNES = 15;
-	    final int NB_COLONNES = 15;
-
+	public void display() {
 	    System.out.print("┏");
-	    for (int i = 0; i < NB_COLONNES; i++) {
+	    for (int i = 0; i < SIZE; i++) {
 	        System.out.print("━━━━━┳");
 	    }
 	    System.out.println("━━━━━┓");
 
-	    for (int i = 0; i < NB_LIGNES; i++) {
-	        System.out.print("┣");
-	        for (int j = 0; j < NB_COLONNES; j++) {
-	            System.out.print("━━━━━╋");
+	    for (int i = 0; i < SIZE; i++) {
+	        System.out.print("┃");
+	        for (int j = 0; j < SIZE; j++) {
+	            Position position = new Position(i, j);
+	            Box box = this.boxes.get(position);
+	            String tokenDisplay = " ";
+	            if (box != null && box.getToken() != null) {
+	                tokenDisplay = box.getToken().display(); 
+	            }
+	            System.out.printf("  %s  ┃", tokenDisplay);
 	        }
-	        System.out.println("━━━━━┫");
+	        System.out.println();
+	        if (i < SIZE - 1) {
+	            System.out.print("┣");
+	            for (int j = 0; j < SIZE; j++) {
+	                System.out.print("━━━━━╋");
+	            }
+	            System.out.println("━━━━━┫");
+	        }
 	    }
 
 	    System.out.print("┗");
-	    for (int i = 0; i < NB_COLONNES; i++) {
+	    for (int i = 0; i < SIZE; i++) {
 	        System.out.print("━━━━━┻");
 	    }
 	    System.out.println("━━━━━┛");
+	}
+	
+	public void setToken(Position pos, Token token) {
+		Box box = this.boxes.get(pos);
+		
+		box.setToken(token);
 	}
 }
