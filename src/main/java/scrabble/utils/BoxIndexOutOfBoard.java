78577b6609ab8ddad7b8c8e3891969db6ee65e56
Lucas Bocquet <159040687+lbocquet0@users.noreply.github.com>
2024-05-23 16:09:17 +0200

    feat: add concerned Box row, column in Exception

--- src/main/java/scrabble/utils/BoxIndexOutOfBoard.java
+++ src/main/java/scrabble/utils/BoxIndexOutOfBoard.java
@@ -1,7 +1,20 @@
 package scrabble.utils;
 
 public class BoxIndexOutOfBoard extends Exception {
-	public BoxIndexOutOfBoard() {
+	private Integer row;
+	private Integer column;
+
+	public BoxIndexOutOfBoard(Integer row, Integer column) {
 		super("The box index is out of the board.");
+		this.row = row;
+		this.column = column;
+	}
+
+	public Integer getRow() {
+		return this.row;
+	}
+
+	public Integer getColumn() {
+		return this.column;
 	}
 }
