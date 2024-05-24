59845beeffdbe303e53073444cec36cee23fd86d
Hugo H. <51268820+hugoheml@users.noreply.github.com>
2024-05-24 11:23:05 +0200

    wip: start countScore method in ScoreCounter

---
+++ src/main/java/scrabble/utils/ScoreCounter.java
@@ -0,0 +1,17 @@
+package scrabble.utils;
+
+import java.util.ArrayList;
+
+import scrabble.model.board.Action;
+import scrabble.model.board.Board;
+
+public class ScoreCounter {
+
+	public static Integer countScore(Board board, ArrayList<Action> actions, Direction direction) {
+		Integer score = 0;
+
+		
+
+		return score;
+	}
+}
