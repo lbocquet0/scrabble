8ac47448b6563aaabcfd6b0dceae3b21c3da58a0
fettle-mehatab <51268820+hugoheml@users.noreply.github.com>
2024-05-25 14:35:56 +0200

    feat: add JokerTest

---
+++ src/test/java/JokerTest.java
@@ -0,0 +1,73 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import scrabble.model.token.FrenchLetter;
+import scrabble.model.token.Joker;
+
+public class JokerTest {
+
+	Joker joker = new Joker();
+	@BeforeEach
+	public void initialize() {
+		joker = new Joker();
+	}
+
+	@Test
+	public void shouldReturnTrueIfJoker() {		
+		boolean isJoker = joker.isJoker();
+
+		assertTrue(isJoker);
+	}
+
+	@Test
+	public void testDescribeJokerWhenDoesntContainsLetter() {
+		String display = joker.display();
+
+		assertEquals("JOK", display);
+	}
+
+	@Test
+	public void testGetScoreJoker() {	
+		Integer score = joker.getScore();
+
+		assertEquals(0, score);
+	}
+
+	@Test
+	public void testSetLetter() {
+		FrenchLetter letter = FrenchLetter.A;
+		
+		joker.setLetter(letter);
+		
+		FrenchLetter getLetterResult = joker.getLetter();
+		assertEquals(letter, getLetterResult);
+	}
+
+	@Test
+	public void shouldReturnFalseIfDoesntContainsLetter() {
+		
+		boolean haveLetter = joker.haveLetter();
+		assertFalse(haveLetter);
+	}
+	
+	@Test
+	public void shouldReturnTrueIfContainsLetter() {
+		FrenchLetter letter = FrenchLetter.A;
+		joker.setLetter(letter);
+		
+		boolean haveLetter = joker.haveLetter();
+		assertTrue(haveLetter);
+	}
+	
+	@Test
+	public void testGetLetterNull() {
+		
+		FrenchLetter letter = joker.getLetter();
+		assertNull(letter);
+	}
+}
