3ec3c598e2a851daf7ecb2f724ab4ee99bfbca75
Lucas Bocquet <159040687+lbocquet0@users.noreply.github.com>
2024-05-26 11:26:57 +0200

    feat: add letterAround method to board

--- src/main/java/scrabble/model/board/Board.java
+++ src/main/java/scrabble/model/board/Board.java
@@ -61,6 +61,30 @@ public class Board {
 		this.actionHistory.add(new Action(row, column, box));
 	}
 
+	public Boolean isLetterAround(Integer row, Integer column) {
+		Boolean isLetterAround = false;
+		// Check if there is a letter on side of boxes but not on diagonal and the boxes
+		for (int i = row - 1; i <= row + 1; i++) {
+			for (int j = column - 1; j <= column + 1; j++) {
+				if (i >= 1 && i <= SIZE && j >= 1 && j <= SIZE) {
+					if (i != row || j != column) {
+						if ((i == row && j != column) || (i != row && j == column)) {
+							try {
+								if (!this.getBox(i, j).isEmpty()) {
+									isLetterAround = true;
+								}
+							} catch (BoxIndexOutOfBoard e) {
+								e.printStackTrace();
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return isLetterAround;
+	}
+
 	public ArrayList<Token> cancelLastAction() {
 		return this.actionHistory.undoAllActions();
 	}
