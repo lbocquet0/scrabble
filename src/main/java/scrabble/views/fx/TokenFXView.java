ac4649273097b9bca2b7641c1ef4b0b1130294bd
Hugo H. <51268820+hugoheml@users.noreply.github.com>
2024-06-05 16:30:00 +0200

    feat: add a new design on TokenFXView

--- src/main/java/scrabble/views/fx/TokenFXView.java
+++ src/main/java/scrabble/views/fx/TokenFXView.java
@@ -1,23 +1,25 @@
 package scrabble.views.fx;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
-import javafx.scene.text.TextAlignment;
 import scrabble.model.token.Token;
 
-public class TokenFXView extends HBox implements FXView {
+public class TokenFXView extends StackPane implements FXView {
 	private final Token token;
 
-	private static int TOKEN_SIZE = 50;
-	private static int LETTER_FONT_SIZE = 20;
-	private static int POINTS_FONT_SIZE = 15;
+	public static final int TOKEN_SIZE = 50;
+	private static final int LETTER_FONT_SIZE = 20;
+	private static final int POINTS_FONT_SIZE = 10;
 
 	public TokenFXView(Token token) {
 		super();
 		this.token = token;
 
 		this.setPrefSize(TOKEN_SIZE, TOKEN_SIZE);
+		this.setStyle("-fx-border-color: black; -fx-border-width: 1;");
 		
 		this.updateView();
 	}
@@ -27,16 +29,16 @@ public class TokenFXView extends HBox implements FXView {
 
 		Label label = new Label(token.getLetter().toString());
 		label.setFont(new Font(LETTER_FONT_SIZE));
-		label.setTextAlignment(TextAlignment.CENTER);
 		
 		Label pointsLabel = new Label(Integer.toString(token.getScore()));
 		pointsLabel.setFont(new Font(POINTS_FONT_SIZE));
-		pointsLabel.setTextAlignment(TextAlignment.CENTER);
-		pointsLabel.setTranslateY(LETTER_FONT_SIZE);
 		
 		this.getChildren().add(label);
 		this.getChildren().add(pointsLabel);
 
-		this.setStyle("-fx-border-color: black; -fx-border-width: 1;");
+		StackPane.setAlignment(label, Pos.CENTER);
+		StackPane.setAlignment(pointsLabel, Pos.BOTTOM_RIGHT);
+
+		StackPane.setMargin(pointsLabel, new Insets(0, 10, 7.5, 0));
 	}
 }
