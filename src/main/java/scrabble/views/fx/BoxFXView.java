2fbd167c75ac3ee4c842c14a5bfa5cffeb8f12eb
Hugo H. <51268820+hugoheml@users.noreply.github.com>
2024-06-05 16:56:14 +0200

    feat: add middle box support on Board

--- src/main/java/scrabble/views/fx/BoxFXView.java
+++ src/main/java/scrabble/views/fx/BoxFXView.java
@@ -1,16 +1,21 @@
 package scrabble.views.fx;
 
-import javafx.scene.layout.HBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import scrabble.model.board.Box;
 import scrabble.model.token.Token;
 
-public class BoxFXView extends HBox implements FXView {
+public class BoxFXView extends StackPane implements FXView {
 	private final Box box;
 
 	public BoxFXView(Box box) {
 		super();
 
 		this.box = box;
+		this.setPrefSize(TokenFXView.TOKEN_SIZE, TokenFXView.TOKEN_SIZE);
+
+		this.updateView();
 	}
 	
 	public void updateView() {
@@ -23,6 +28,22 @@ public class BoxFXView extends HBox implements FXView {
 			TokenFXView tokenFXView = new TokenFXView(token);
 
 			this.getChildren().add(tokenFXView);
+		
+		} else if (this.box.isMiddle()) {
+			System.out.println("ok");
+
+			ImageView middleImageView = this.getMiddleImageView();
+			this.getChildren().add(middleImageView);
 		}
+	}
+
+	private ImageView getMiddleImageView() {
+		Image image = new Image("file:src/main/resources/images/star.png");
+
+		ImageView imageView = new ImageView(image);
+		imageView.setFitWidth(TokenFXView.TOKEN_SIZE);
+		imageView.setFitHeight(TokenFXView.TOKEN_SIZE);
+
+		return imageView;
 	}
 }
