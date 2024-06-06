7340b0af3f6a8359bb87a09363b36c8bc8c9d968
Lucas Bocquet <159040687+lbocquet0@users.noreply.github.com>
2024-06-06 10:41:30 +0200

    feat: normalPlayWord popup method

--- src/main/java/scrabble/application/ScrabbleApplicationFX.java
+++ src/main/java/scrabble/application/ScrabbleApplicationFX.java
@@ -70,6 +70,42 @@ public class ScrabbleApplicationFX extends Application {
 		primaryStage.setScene(new Scene(root, 1920, 1080));
 	}
 	
+	private static void normalPlayWord() {
+		Alert alert = new Alert(Alert.AlertType.NONE);
+		alert.setTitle("Jouer un mot");
+		alert.setHeaderText("Entrez la position initiale");
+
+		GridPane grid = new GridPane();
+		grid.setHgap(10);
+		grid.setVgap(10);
+
+		TextField line = new TextField();
+		line.setPromptText("Ligne");
+		TextField column = new TextField();
+		column.setPromptText("Colonne");
+
+		grid.add(new Label("Ligne :"), 0, 0);
+		grid.add(line, 1, 0);
+		grid.add(new Label("Colonne :"), 0, 1);
+		grid.add(column, 1, 1);
+
+		grid.add(new Label("Direction :"), 0, 2);
+		ChoiceBox<Object> direction = new ChoiceBox<Object>();
+		direction.getItems().addAll(Direction.HORIZONTAL, Direction.VERTICAL);
+		direction.setValue(Direction.HORIZONTAL);
+		grid.add(direction, 1, 2);
+
+
+		alert.getDialogPane().getButtonTypes().add(ButtonType.OK);
+		alert.getDialogPane().setContent(grid);
+
+		Optional<ButtonType> result = alert.showAndWait();
+		if (result.isPresent()) {
+			int x = Integer.parseInt(line.getText());
+			int y = Integer.parseInt(column.getText());
+			Direction dir = direction.getValue() == Direction.HORIZONTAL ? Direction.HORIZONTAL : Direction.VERTICAL;
+		}
+	}
 	@Override
 	public void start(Stage primaryStage) throws EmptyBagException {
 		primaryStage.setTitle("Scrabble");
