4664b6d6a39eecbe1d0bcf33343b7c6d37f1eba6
Eliott Bayart <eliott.bayart@etu.unilim.fr>
2024-06-08 21:01:21 +0200

    refact: modify existing ScoreCounter Tests / add other test for multipliers

--- src/test/java/scrabble/utils/ScoreCounterTest.java
+++ src/test/java/scrabble/utils/ScoreCounterTest.java
@@ -1,7 +1,6 @@
 package scrabble.utils;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,7 +8,9 @@ import org.junit.jupiter.api.Test;
 import scrabble.model.board.Board;
 import scrabble.model.board.Box;
 import scrabble.model.board.action.Action;
+import scrabble.model.board.action.ActionHistory;
 import scrabble.model.token.FrenchLetter;
+import scrabble.model.token.Joker;
 import scrabble.model.token.Token;
 import scrabble.utils.exceptions.PositionOutOfBoard;
 import scrabble.utils.exceptions.EmptyBoxException;
@@ -18,116 +19,290 @@ public class ScoreCounterTest {
 
 	private Token tokenA;
 	private Board board;
+	private ActionHistory actionHistory;
 
 	@BeforeEach
 	public void initialize() {
 		board = new Board();
 		tokenA = new Token(FrenchLetter.A);
-	}
-
-	@Test
-	public void shouldCountWordScore() {
-		Box box = new Box(false, tokenA);
-		Box box2 = new Box(false, tokenA);
-
-		ArrayList<Box> wordLetters = new ArrayList<>();
-		wordLetters.add(box);
-		wordLetters.add(box2);
-
-		Integer score = ScoreCounter.countWordScore(wordLetters);
-
-		assertEquals(FrenchLetter.A.getPoint() * 2, score);
+		actionHistory = board.getActionsHistory();
 	}
 
 	@Test
 	public void should_return_0_when_no_action() {
-		ArrayList<Action> wordLetters = new ArrayList<Action>();
-		Integer score = ScoreCounter.countScore(board, wordLetters, Direction.HORIZONTAL);
+		Integer score = ScoreCounter.countScore(board, Direction.HORIZONTAL);
 		assertEquals(0, score);
 	}
 
 	@Test
-	public void should_return_0_when_no_direction() {
-		ArrayList<Action> wordLetters = new ArrayList<Action>();
-		Integer score = ScoreCounter.countScore(board, wordLetters, null);
-		assertEquals(0, score);
-	}
+	public void should_count_score_when_word_is_aaa_horizontal() throws PositionOutOfBoard, EmptyBoxException {
+		board.setToken(tokenA, 8, 7);
+		board.setToken(tokenA, 8, 8);
+		board.setToken(tokenA, 8, 9);
 
-	@Test
-	public void should_return_3_when_word_is_aaa() throws EmptyBoxException, PositionOutOfBoard {
-		Box box = new Box(false, tokenA);
-		Box box2 = new Box(false, tokenA);
-		Box box3 = new Box(false, tokenA);
-
-		board.setToken(tokenA,7, 7);
-		board.setToken(tokenA,7, 8);
-		board.setToken(tokenA,7, 9);
-
-		ArrayList<Action> actions = new ArrayList<Action>();
-		actions.add(new Action(7, 7, box));
-		actions.add(new Action(7, 8, box2));
-		actions.add(new Action(7, 9, box3));
-
-		Integer score = ScoreCounter.countScore(board, actions, Direction.HORIZONTAL);
+		Integer score = ScoreCounter.countScore(board, Direction.HORIZONTAL);
 		assertEquals(3, score);
 	}
 
 	@Test
-	public void should_return_7_when_word_is_aaaa_and_completing_aa_vertically() throws EmptyBoxException, PositionOutOfBoard {
-		Box box = new Box(false, tokenA);
-		Box box2 = new Box(false, tokenA);
-		Box box3 = new Box(false, tokenA);
-		Box box4 = new Box(false, tokenA);
-		Box box5 = new Box(false, tokenA);
-
-		board.setToken(tokenA, 6, 9);
-
-		board.setToken(tokenA, 7, 7);
+	public void should_count_score_when_word_is_aaa_vertical() throws PositionOutOfBoard, EmptyBoxException {
 		board.setToken(tokenA, 7, 8);
-		board.setToken(tokenA, 7, 9);
-		board.setToken(tokenA, 7, 10);
-		board.setToken(tokenA, 7, 11);
+		board.setToken(tokenA, 8, 8);
+		board.setToken(tokenA, 9, 8);
 
-		ArrayList<Action> actions = new ArrayList<Action>();
-		actions.add(new Action(7, 7, box));
-		actions.add(new Action(7, 8, box2));
-		actions.add(new Action(7, 9, box3));
-		actions.add(new Action(7, 10, box4));
-		actions.add(new Action(7, 11, box5));
-
-		Integer score = ScoreCounter.countScore(board, actions, Direction.HORIZONTAL);
-		assertEquals(7, score);
+		Integer score = ScoreCounter.countScore(board, Direction.VERTICAL);
+		assertEquals(3, score);
 	}
 
 	@Test
-	public void should_return_57_when_word_is_aaaaaaa() throws EmptyBoxException, PositionOutOfBoard {
-		Box box = new Box(false, tokenA);
-		Box box2 = new Box(false, tokenA);
+	public void should_count_score_when_complete_word_horizontal() throws PositionOutOfBoard, EmptyBoxException {
 		Box box3 = new Box(false, tokenA);
-		Box box4 = new Box(false, tokenA);
-		Box box5 = new Box(false, tokenA);
-		Box box6 = new Box(false, tokenA);
-		Box box7 = new Box(false, tokenA);
 
+		board.setToken(tokenA, 7, 8);
+		board.setToken(tokenA, 8, 8);
+		board.setToken(tokenA, 9, 8);
+		actionHistory.clear();
+
+		actionHistory.add(new Action(9, 8, box3));
+
+		Integer score = ScoreCounter.countScore(board, Direction.VERTICAL);
+		assertEquals(3, score);
+	}
+
+	@Test
+	public void should_count_score_when_complete_word_vertical() throws PositionOutOfBoard, EmptyBoxException {
+		Box box3 = new Box(false, tokenA);
+
+		board.setToken(tokenA, 8, 7);
+		board.setToken(tokenA, 8, 8);
+		board.setToken(tokenA, 8, 9);
+		actionHistory.clear();
+
+		actionHistory.add(new Action(8, 9, box3));
+
+		Integer score = ScoreCounter.countScore(board, Direction.HORIZONTAL);
+		assertEquals(3, score);
+	}
+
+	@Test
+	public void should_count_score_when_add_horizontal_word_across_vertical_word()
+			throws PositionOutOfBoard, EmptyBoxException {
+		board.setToken(tokenA, 7, 7);
+		board.setToken(tokenA, 8, 7);
+		board.setToken(tokenA, 9, 7);
+		actionHistory.clear();
+
+		board.setToken(tokenA, 8, 6);
+		board.setToken(tokenA, 8, 8);
+
+		Integer score = ScoreCounter.countScore(board, Direction.HORIZONTAL);
+		assertEquals(3, score);
+	}
+
+	@Test
+	public void should_count_score_when_add_vertical_word_across_horizontal_word()
+			throws PositionOutOfBoard, EmptyBoxException {
 		board.setToken(tokenA, 7, 7);
 		board.setToken(tokenA, 7, 8);
 		board.setToken(tokenA, 7, 9);
-		board.setToken(tokenA, 7, 10);
-		board.setToken(tokenA, 7, 11);
-		board.setToken(tokenA, 7, 12);
-		board.setToken(tokenA, 7, 13);
+		actionHistory.clear();
 
-		ArrayList<Action> actions = new ArrayList<Action>();
-		actions.add(new Action(7, 7, box));
-		actions.add(new Action(7, 8, box2));
-		actions.add(new Action(7, 9, box3));
-		actions.add(new Action(7, 10, box4));
-		actions.add(new Action(7, 11, box5));
-		actions.add(new Action(7, 12, box6));
-		actions.add(new Action(7, 13, box7));
+		board.setToken(tokenA, 6, 8);
+		board.setToken(tokenA, 8, 8);
 
-		Integer score = ScoreCounter.countScore(board, actions, Direction.HORIZONTAL);
-		assertEquals(57, score);
+		Integer score = ScoreCounter.countScore(board, Direction.VERTICAL);
+		assertEquals(3, score);
 	}
 
+	@Test
+	public void should_count_score_when_create_new_words_with_vertical_and_horizontal_words()
+			throws PositionOutOfBoard, EmptyBoxException {
+		board.setToken(tokenA, 6, 8);
+		board.setToken(tokenA, 7, 8);
+
+		board.setToken(tokenA, 8, 6);
+		board.setToken(tokenA, 8, 7);
+
+		actionHistory.clear();
+
+		board.setToken(tokenA, 8, 8);
+
+		Integer score = ScoreCounter.countScore(board, Direction.HORIZONTAL);
+		assertEquals(6, score);
+	}
+
+	@Test
+	public void should_count_score_for_two_words_both_horizontal() throws PositionOutOfBoard, EmptyBoxException {
+		board.setToken(tokenA, 8, 6);
+		board.setToken(tokenA, 8, 7);
+		actionHistory.clear();
+
+		board.setToken(tokenA, 8, 9);
+		board.setToken(tokenA, 8, 10);
+		actionHistory.clear();
+
+		board.setToken(tokenA, 8, 8);
+
+		Integer score = ScoreCounter.countScore(board, Direction.HORIZONTAL);
+		assertEquals(5, score);
+	}
+
+	@Test
+	public void should_count_score_for_two_words_both_vertical() throws PositionOutOfBoard, EmptyBoxException {
+		board.setToken(tokenA, 6, 8);
+		board.setToken(tokenA, 7, 8);
+		actionHistory.clear();
+
+		board.setToken(tokenA, 9, 8);
+		board.setToken(tokenA, 10, 8);
+		actionHistory.clear();
+
+		board.setToken(tokenA, 8, 8);
+
+		Integer score = ScoreCounter.countScore(board, Direction.VERTICAL);
+		assertEquals(5, score);
+	}
+
+	@Test
+	public void should_count_score_with_double_letter_score() throws PositionOutOfBoard, EmptyBoxException {
+		board.setToken(tokenA, 1, 4);
+		board.setToken(tokenA, 1, 5);
+		board.setToken(tokenA, 1, 6);
+
+		Integer score = ScoreCounter.countScore(board, Direction.HORIZONTAL);
+		assertEquals(4, score);
+	}
+
+	@Test
+	public void should_count_score_with_triple_letter_score() throws PositionOutOfBoard, EmptyBoxException {
+		board.setToken(tokenA, 2, 6);
+		board.setToken(tokenA, 2, 7);
+		board.setToken(tokenA, 2, 8);
+
+		Integer score = ScoreCounter.countScore(board, Direction.HORIZONTAL);
+		assertEquals(5, score);
+	}
+
+	@Test
+	public void should_count_score_with_double_word_score() throws PositionOutOfBoard, EmptyBoxException {
+		board.setToken(tokenA, 2, 2);
+		board.setToken(tokenA, 2, 3);
+		board.setToken(tokenA, 2, 4);
+
+		Integer score = ScoreCounter.countScore(board, Direction.HORIZONTAL);
+		assertEquals(6, score);
+	}
+
+	@Test
+	public void should_count_score_with_triple_word_score() throws PositionOutOfBoard, EmptyBoxException {
+		board.setToken(tokenA, 1, 1);
+		board.setToken(tokenA, 1, 2);
+		board.setToken(tokenA, 1, 3);
+
+		Integer score = ScoreCounter.countScore(board, Direction.HORIZONTAL);
+		assertEquals(9, score);
+	}
+
+	@Test
+	public void should_count_score_with_double_letter_and_double_word_score()
+			throws PositionOutOfBoard, EmptyBoxException {
+		board.setToken(tokenA, 1, 4);
+		board.setToken(tokenA, 2, 4);
+		board.setToken(tokenA, 3, 4);
+		board.setToken(tokenA, 4, 4);
+
+		Integer score = ScoreCounter.countScore(board, Direction.VERTICAL);
+		assertEquals(10, score);
+	}
+
+	@Test
+	public void should_count_score_when_complete_word_with_double_letter_effect()
+			throws PositionOutOfBoard, EmptyBoxException {
+		board.setToken(tokenA, 7, 7);
+		board.setToken(tokenA, 8, 7);
+		board.setToken(tokenA, 9, 7);
+		actionHistory.clear();
+
+		board.setToken(tokenA, 10, 7);
+
+		Integer score = ScoreCounter.countScore(board, Direction.VERTICAL);
+		assertEquals(4, score);
+	}
+
+	@Test
+	public void should_count_score_when_complete_word_with_double_word_effect()
+			throws PositionOutOfBoard, EmptyBoxException {
+		board.setToken(tokenA, 1, 1);
+		board.setToken(tokenA, 2, 1);
+
+		actionHistory.clear();
+
+		board.setToken(tokenA, 3, 1);
+
+		Integer score = ScoreCounter.countScore(board, Direction.VERTICAL);
+		assertEquals(3, score);
+	}
+
+	@Test
+	public void should_count_score_when_complete_two_words_with_double_letter_effect()
+			throws PositionOutOfBoard, EmptyBoxException {
+		board.setToken(tokenA, 1, 1);
+		board.setToken(tokenA, 2, 1);
+		board.setToken(tokenA, 3, 1);
+		actionHistory.clear();
+
+		board.setToken(tokenA, 4, 2);
+		board.setToken(tokenA, 4, 3);
+		actionHistory.clear();
+
+		board.setToken(tokenA, 4, 1);
+
+		Integer score = ScoreCounter.countScore(board, Direction.VERTICAL);
+		assertEquals(9, score);
+	}
+
+	@Test
+	public void should_count_score_when_complete_two_words_with_double_word_effect()
+			throws PositionOutOfBoard, EmptyBoxException {
+		board.setToken(tokenA, 4, 1);
+		board.setToken(tokenA, 4, 2);
+		board.setToken(tokenA, 4, 3);
+		actionHistory.clear();
+
+		board.setToken(tokenA, 1, 4);
+		board.setToken(tokenA, 2, 4);
+		board.setToken(tokenA, 3, 4);
+		actionHistory.clear();
+
+		board.setToken(tokenA, 4, 4);
+
+		Integer score = ScoreCounter.countScore(board, Direction.HORIZONTAL);
+		assertEquals(16, score);
+	}
+
+	@Test
+	public void should_count_score_when_play_joker() throws PositionOutOfBoard, EmptyBoxException {
+		Joker joker = new Joker();
+		joker.setLetter(FrenchLetter.A);
+
+		board.setToken(joker, 8, 8);
+		board.setToken(tokenA, 8, 7);
+
+		Integer score = ScoreCounter.countScore(board, Direction.HORIZONTAL);
+		assertEquals(1, score);
+	}
+
+	@Test
+	public void should_count_score_when_play_all_rack_letters() throws PositionOutOfBoard, EmptyBoxException {
+		board.setToken(tokenA, 8, 8);
+		board.setToken(tokenA, 8, 7);
+		board.setToken(tokenA, 8, 5);
+		board.setToken(tokenA, 8, 6);
+		board.setToken(tokenA, 8, 9);
+		board.setToken(tokenA, 8, 10);
+		board.setToken(tokenA, 8, 11);
+
+		Integer score = ScoreCounter.countScore(board, Direction.HORIZONTAL);
+		assertEquals(57, score);
+	}
 }
